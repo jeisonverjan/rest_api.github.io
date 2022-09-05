f4c26b56f82eb5c0f9c068f511ef071607b77a9e
Verjan <jeisonverjan@gmail.com>
2022-09-05 16:29:48 -0500

    adding functionalities

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import json
 from typing import List
-from fastapi import FastAPI, Path, HTTPException, status
+from fastapi import FastAPI, Path, HTTPException
 from pydantic import BaseModel
+from random import randint
 
 class User(BaseModel):
     name: str
@@ -10,10 +11,14 @@ class User(BaseModel):
     born: str
     live: bool
     photo: str
-    geder: str
+    gender: str
     tags: List
 
-app = FastAPI()
+app = FastAPI(
+    title="Thousand celebrities - API.",
+    description="In this API you can obtain information about thousands of celebrities of all times in different categories. You can nominate a new celebrity, which after verification will be added; additionally, you can request to update or modify information of existing celebrities."
+    
+)
 
 with open("celebrities.json", "r") as f:
     data = json.load(f)
@@ -21,29 +26,75 @@ with open("celebrities.json", "r") as f:
 index = len(data)
 
 categories = {}
-for index, categ in enumerate(data):
-    if categ['category'] not in categories.values():
-        categories[index] = categ['category']
+cont = 1
+for categ in data:
+    if data[categ]['category'] not in categories.values():
+        categories[cont] = data[categ]['category']
+        cont += 1
 
-@app.get('/')
-def home():
+@app.get('/', tags=['Get data'])
+async def home():
     return data
 
-@app.get('/categories')
-def home():
+@app.get('/categories', tags=['Get data'])
+async def home():
     return categories
 
-@app.get("/get-user/{user_id}")
-def get_user(user_id: int = Path(None, description="From 0 to 5", gt=-1)):
+@app.get("/limit/{number}", tags=['Get data'])
+async def get_user_by_limit(number: int = Path(None, description="Get random celebrities, enter the number of records you want.", gt=0, lt=index+1)):
+    data_limit = {}
+    while len(data_limit) < number:
+        random_num = str(randint(1, len(data)))
+        data_limit[random_num] = data[random_num]
+    return data_limit
+
+@app.get("/get-user/{user_id}", tags=['Get data'])
+async def get_user_by_id(user_id: int = Path(None, description="Get a celebrity by ID, enter the ID you want to get.", gt=0, lt=index+1)):
     global index
-    if user_id > index:
+    if str(user_id) not in data.keys():
         raise HTTPException(status_code=400, detail="User not found.")
 
-    return data[user_id]
+    return data[str(user_id)]
 
-@app.post("/create-user")
-def create_user(user: User):
+
+@app.post("/create-celebrity", tags=['Add new celebrity request'])
+async def create_celebrity(user: User):
+    file_name = "celebrities_requests.json"
+    celebrity_request = {"name": user.name, "country": user.country, "category":user.category, "born": user.born, "live":user.live, "photo":user.photo, "gender":user.gender, "tags": user.tags}
+    with open(file_name, "r") as file:
+        info = json.load(file)
+    cont_file = len(info)
+    info[str(cont_file)] = celebrity_request
+    with open(file_name, "w") as file:
+        json.dump(info, file)
+
+    return "Your request has been received, after verification the celebrity will be added.", info[str(cont_file)]
+
+@app.get('/celebrity-request', tags=['Add new celebrity request'])
+async def celebrities_requests():
+    with open("celebrities_requests.json", "r") as file:
+        data = json.load(file)
+    return data
+
+
+@app.post("/celebrity-update/{user_id}", tags=['Celebrity Update request'])
+async def celebrity_update(user: User, user_id: int = Path(None, description="Enter the ID you want to modify and fill the fileds to update.", gt=0)):
     global index
-    data.append(user)
-    index +=1
-    return data[index]
+    if str(user_id) not in data.keys():
+        raise HTTPException(status_code=400, detail="User not found.")
+    file_name = "celebrities_update.json"
+    celebrity_update = {"name": user.name, "country": user.country, "category":user.category, "born": user.born, "live":user.live, "photo":user.photo, "gender":user.gender, "tags": user.tags}
+    with open(file_name, "r") as file:
+        info = json.load(file)
+    info[str(user_id)] = celebrity_update
+    with open(file_name, "w") as file:
+        json.dump(info, file)
+
+    return "Your request has been received, after verification the celebrity will be updated.", info[str(user_id)]
+
+
+@app.get('/update-requests', tags=['Celebrity Update request'])
+async def update_requests():
+    with open("celebrities_update.json", "r") as file:
+        data = json.load(file)
+    return data
